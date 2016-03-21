a479e4916fc880aeadb62af6ca0ae0804309ee82
agramatir <litvinenko.margarita.77@gmail.com>
2016-03-22 00:33:57 +0200

    add number and value support

--- app/js/controllers/MainPageController.js
+++ app/js/controllers/MainPageController.js
@@ -43,20 +43,25 @@ function MainPageController ($scope, Tree)
 		{
 			$scope.showContext = false;
 			$scope.showRemoveContext = false;
+			$scope.showEditContext = false;
 			$scope.selectedNode = new Object();
 			return;
 		}
 		if(node.children.length > 0)
 		{
 			$scope.showRemoveContext = true;
+			$scope.showEditContext = false;
 			$scope.showContext = false;
 			return;
 		}
 		if(node.type == 'variable' || node.type == 'number')
 		{
-			$scope.showEditContext;
+			$scope.selectedNode = node;
+			$scope.showEditContext = true;
+			return;
 		}
 		$scope.showRemoveContext = false;
+		$scope.showEditContext = false;
 		$scope.selectedNode = node;
 		$scope.context = Tree.getPossibleChildren(node.label);
 		if(!$scope.context.length)
@@ -67,6 +72,15 @@ function MainPageController ($scope, Tree)
 		$scope.showContext = true;
 		$scope.selectedOption = $scope.context[0];
 	};
+
+	$scope.valueNodeLabel = null;
+
+	$scope.addValue = function()
+	{
+		$scope.selectedNode.label = $scope.valueNodeLabel;
+		$scope.showEditContext = false;
+		$scope.valueNodeLabel = null;
+	}
 
 	$scope.addNodes = function()
 	{
