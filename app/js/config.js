c85d4e91a26e47f1c2010263992f851915a3669f
agramatir <litvinenko.margarita.77@gmail.com>
2016-03-21 18:32:19 +0200

    fixes

--- app/js/config.js
+++ app/js/config.js
@@ -3,12 +3,29 @@
 angular
   .module('spLab.config', ['ngRoute'])
   .config(['$routeProvider', manageAppConfig])
+  .constant('config', 
+    {
+      apiUrl : 'http://localhost:8080'
+    });
 
   function manageAppConfig($routeProvider)
   {
     $routeProvider.
-      otherwise({
-        redirectTo: '/',
+      /*when('/buildings', {
+        templateUrl: 'partials/buildings-list.html',
+        controller: 'BuildingsListController'
+      }).
+      when('/buildings/:buildingId/main', {
+        templateUrl: 'partials/building-main.html',
+        controller: 'BuildingMainController',
+        activeTab: 'main'
+      }).
+      when('/buildings/:buildingId/synonym', {
+        templateUrl: 'partials/building-synonym.html',
+        controller: 'BuildingSynonymController',
+        activeTab: 'synonym'
+      }).*/
+      when('/', {
         templateUrl: 'partials/main-page.html',
         controller: 'MainPageController'
       });
